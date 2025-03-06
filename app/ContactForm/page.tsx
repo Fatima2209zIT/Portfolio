ebc0b3b4d6f7f10b57d2f29ab5e90be181a09d77
Mehmil Zeeshan <mehmilzeeshan125@gmail.com>
2025-03-06 17:27:44 +0500

    Initial commit

---
+++ app/ContactForm/page.tsx
@@ -0,0 +1,13 @@
+
+
+import ContactForm3D from "../components/Contactform3d";
+
+const ContactPage = () => {
+  return (
+    <main className="w-full h-screen">
+      <ContactForm3D />
+    </main>
+  );
+};
+
+export default ContactPage;
---
+++ app/components/Skills/Skills.tsx
@@ -0,0 +1,12 @@
+import Skills3D from "./Skills3D";
+
+const Skills = () => (
+  <div className="py-20 w-full" id="skills">
+    <h1 className="heading">
+      My <span className="text-purple">Skills</span>
+    </h1>
+    <Skills3D />
+  </div>
+);
+
+export default Skills;
